7c83c7af719fef888f1a7070b4f63d4834ce414d
Hanhelle <hanhelle@gmail.com>
2019-06-08 22:36:41 -0500

    Barra de navegaciÃon

---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+class Navigation extends Component {
+render() {
+    return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <h2 className="logo">My porfolio</h2>
+    </nav>
+  );
+}
+}
+
+export default Navigation;
