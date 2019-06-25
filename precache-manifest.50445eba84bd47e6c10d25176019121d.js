8dca44849d93e897793686a5569a980f1a6f1e78
Hanhelle <hanhelle@gmail.com>
2019-06-25 13:08:39 -0500

    Updates

---
+++ precache-manifest.50445eba84bd47e6c10d25176019121d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8214e60cea0e73cd9ef9bce0eca8a4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972c24fe1976851b75f7",
+    "url": "/static/css/main.0cb7027e.chunk.css"
+  },
+  {
+    "revision": "692fe713617183e630cd",
+    "url": "/static/js/2.8dd36d33.chunk.js"
+  },
+  {
+    "revision": "972c24fe1976851b75f7",
+    "url": "/static/js/main.dd49ff28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07dbd226ac625886f69bf7e7f50d76fc",
+    "url": "/static/media/datalovers.07dbd226.png"
+  },
+  {
+    "revision": "db62e71edbaabfb486cd92a382664aa2",
+    "url": "/static/media/flower.db62e71e.png"
+  },
+  {
+    "revision": "54e55bc7e983dcdeff877ef6f03600fd",
+    "url": "/static/media/lake.54e55bc7.jpg"
+  },
+  {
+    "revision": "f04f08989357473f4c5bba4a2ddfd464",
+    "url": "/static/media/my_image.f04f0898.jpg"
+  }
+]);
